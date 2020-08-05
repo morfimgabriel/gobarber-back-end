9d2e27d245bdcc504a668de1c498e16c40813c71
Gabriel <morfimgabriel@gmail.com>
2020-08-05 19:24:37 -0300

    Criado relaçao com tabela de users com appointments

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -10,7 +10,7 @@ import AppointmentsRepository from '../repositories/AppointmentRepository';
  */
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
@@ -20,7 +20,7 @@ interface Request {
  */
 
 class CreateAppointmentService {
-  public async execute({ date, provider }: Request): Promise<Appointment> {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
@@ -33,7 +33,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
