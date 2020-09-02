b4144a35160cc3cafc8d1016fa3093fb36083d06
Gabriel <morfimgabriel@gmail.com>
2020-09-02 20:30:00 -0300

    Isolado components para serem reutilizados em outras paginas ( input e button )

---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
+
+// eslint-disable-next-line react/prop-types
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+  <Container>
+    {Icon && <Icon size={20} />}
+    <input {...rest} />
+  </Container>
+);
+
+export default Input;
